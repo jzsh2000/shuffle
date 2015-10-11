746211d4a0ac8eff0c5919a1b12111b3f9f012da
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2015-10-11 20:28:49 +0800

    Add a nrow parament to randomness-test R script

--- test.R
+++ test.R
@@ -17,14 +17,20 @@ if (!file.exists(args[1])) {
 }
 
 shuffle.file=args[1]
+shuffle.length=as.numeric(args[2])
+if(is.na(shuffle.length)||!is.numeric(shuffle.length)) {
+    shuffle.length = -1
+}
+
+shuffle.out <- read.table(shuffle.file, nrow=shuffle.length)
+shuffle.length <- nrow(shuffle.out)
+# shuffle.out <- as.data.frame(lapply(shuffle.out, function(x){x<-factor(x, levels=cards)}))
+# print(head(shuffle.out))
 
 shape=c("S","H","C","D")
 value=c(2:9,0,"J","Q","K","A")
+# 长度为52的向量，分别表示每张牌
 cards <- unlist(lapply(shape, function(x){paste0(x, value)}))
-
-shuffle.out <- read.table(shuffle.file)
-# shuffle.out <- as.data.frame(lapply(shuffle.out, function(x){x<-factor(x, levels=cards)}))
-# print(head(shuffle.out))
 
 for(pos in 1:26) {
     freq.pos.table <- table(shuffle.out[,pos])
@@ -44,14 +50,14 @@ for(pos in 1:26) {
     }
 }
 
-write.table(freq.frame, paste0(shuffle.file,".test"), sep="\t", quote=FALSE, row.names=FALSE)
+write.table(freq.frame, paste0(shuffle.file,".",shuffle.length,".test"), sep="\t", quote=FALSE, row.names=FALSE)
 
 library(ggplot2, quietly=TRUE, warn.conflicts=FALSE)
 
 plot.obj <- ggplot(freq.frame, aes(x=pos, y=freq, fill=card))+geom_bar(stat="identity", colour="white", width=1)+guides(fill=FALSE)+scale_x_continuous(breaks=1:26)
-ggsave(plot = plot.obj, filename=paste0(shuffle.file,".pdf"), height=6, width=8)
+ggsave(plot = plot.obj, filename=paste0(shuffle.file,".",shuffle.length,".pdf"), height=6, width=8)
 
 plot.obj <- ggplot(freq.frame, aes(x=pos, y=freq, fill=test.conf))+geom_bar(stat="identity", colour="white", width=1)+scale_x_continuous(breaks=1:26)
-ggsave(plot = plot.obj, filename=paste0(shuffle.file,".test.pdf"), height=6, width=8)
+ggsave(plot = plot.obj, filename=paste0(shuffle.file,".",shuffle.length,".test.pdf"), height=6, width=8)
 
 detach('package:ggplot2')
