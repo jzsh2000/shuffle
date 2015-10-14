5e44a2ee1c7c0e2f0e4530ade4ddaa2bb41aba97
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2015-10-14 22:25:58 +0800

    `test.plot.R` able to handle multiple files now

--- test.plot.R
+++ test.plot.R
@@ -15,12 +15,15 @@ if (length(args)==0) {
     usage()
 }
 
-if (!file.exists(args[1])) {
-    cat(paste("Cannot read file:", args[1], "\n"))
-    q()
+library(ggplot2, quietly=TRUE, warn.conflicts=FALSE)
+
+for (testfile in args) {
+
+if (!file.exists(testfile)) {
+    cat(paste0("WARNING: cannot read file: ", testfile, "\n"))
+    next
 }
 
-testfile=args[1]
 # columns: card | freq | pos | test | test.conf
 testdata <- read.delim(testfile)
 
@@ -30,9 +33,9 @@ testdata$test.conf[testdata$test>=0.001]="0.01"
 testdata$test.conf[testdata$test>=0.01]="0.05"
 testdata$test.conf[testdata$test>=0.05]="1"
 
-library(ggplot2, quietly=TRUE, warn.conflicts=FALSE)
-
 plot.obj <- ggplot(testdata, aes(x=pos, y=freq, fill=test.conf))+scale_fill_manual(values=c("0.0001"="red","0.001"="magenta","0.01"="burlywood","0.05"="darkseagreen","1"="deepskyblue"), limits = c("0.0001", "0.001", "0.01", "0.05", "1"))+geom_bar(stat="identity", colour="white", width=1)+scale_x_continuous(breaks=1:max(testdata$pos))
 ggsave(plot = plot.obj, filename=paste0(testfile,".pdf"), height=6, width=8)
+
+}
 
 detach('package:ggplot2')
